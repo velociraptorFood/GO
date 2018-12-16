8f9e984d6c73e808c343808845fa1666301a62dc
velociraptorFood <bsmit@hotmail.nl>
2018-12-16 17:23:35 +0100

    Eval()
    Heb een begin gemaakt aan een eval functie

--- Program.cs
+++ Program.cs
@@ -8,9 +8,13 @@ namespace GO
 {
     class Program
     {
+        static Order[] orderList;
+        static Tuple<int, int>[,] afstanden;
+        static int capacity = 20000;
+
         static void Main()
         {
-            Order[] orderList = new Order[1177];
+            orderList = new Order[1177];
             string input = Console.ReadLine();
             for (int i = 0; i < 1177; i++)
             {
@@ -24,8 +28,9 @@ namespace GO
                 orderList[i] = newOrder;
                 input = Console.ReadLine();
             }
-        
-            Tuple<int, int>[,] afstanden = new Tuple<int, int>[1099, 1099];
+            
+            //afstanden[punt A, punt B] = (afstand, rijtijd)
+            afstanden = new Tuple<int, int>[1099, 1099];
             Console.ReadLine();
             input = Console.ReadLine();
             while (!String.IsNullOrEmpty(input))
@@ -34,6 +39,44 @@ namespace GO
                 afstanden[int.Parse(split[0]), int.Parse(split[1])] = new Tuple<int, int>(int.Parse(split[2]), int.Parse(split[3]));
                 input = Console.ReadLine();
             }
+
+            Console.WriteLine(Eval(Start()));
+        }
+
+        static List<Order> Start()
+        {
+            List<Order> start = orderList.ToList();
+            return start;
+        }
+
+        static float Eval(List<Order> input)
+        {
+            float score = 0;
+            float currentLoad = 0;   
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                score += input[i].ledigingsDuur;
+                currentLoad += input[i].volume * 0.2f;
+
+                //720 min per dag zijn de autos beschikbaar
+                float nextDestTime = afstanden[input[i].matrixID, input[i++].matrixID].Item2;
+                if(score % 720 + nextDestTime + afstanden[input[i++].matrixID, 287].Item2 + 30 >= 720)
+                {
+                    score += afstanden[input[i].matrixID, 287].Item2 + 30;
+                }
+                else if (currentLoad + input[i++].volume * 0.2f > capacity)
+                {
+                    //naar stort en storten
+                    score += afstanden[input[i].matrixID, 287].Item2 + 30;
+                    currentLoad = 0;
+                    //naar volgende bestemming
+                    score += afstanden[287, input[i++].matrixID].Item2;
+                }
+                else
+                    score += nextDestTime;                
+            }
+            score += afstanden[input[input.Count - 1].matrixID, 287].Item2;
+            return score;
         }
     }
 }
